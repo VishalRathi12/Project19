adaccc4d4e6fa841e8d274e2e2583c9a9d0a950f
Vishal Rathi <vishalrathi46@gmail.com>
2025-03-08 22:40:47 +0530

    second commit

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('Open Google and check title', async ({ page }) => {
+test('Open Google and check title',{tag:['@PlaywrightWithJenkins'] }, async ({ page }) => {
     await page.goto('https://www.google.com');
     await expect(page).toHaveTitle(/Google/);
 });
